bdcfada98aced7e52b1c770b3cc3fcb6945a1f45
Wassim <115497440+ZayToxFF@users.noreply.github.com>
2023-12-29 21:24:50 +0100

    fix test lastt

--- app.py
+++ app.py
@@ -1,30 +1,29 @@
 from flask import Flask, render_template, request
 import random
-import time
 
 app = Flask(__name__)
 i = 1
 pseudo = "Guest"
 message = ""
-cible = 0  # Déclarer cible en dehors des routes pour éviter des erreurs
+cible = 0
+
+def generate_target():
+    return random.randint(1, 100)
 
 @app.route('/')
 def debut():
-    global i,cible
+    global i, cible
     i = 1
-    cible = random.randint(1, 100)
-
+    cible = generate_target()
     return render_template('index.html', cible=cible)
 
 @app.route('/loading', methods=['GET', 'POST'])
 def loading_page():
-    global pseudo,cible
-    
+    global pseudo, cible
 
     if request.method == 'POST':
         pseudo = request.form['pseudo']
 
-    time.sleep(2)
     return render_template('loading.html', pseudo=pseudo, cible=cible)
 
 @app.route('/essai', methods=['GET', 'POST'])
@@ -34,17 +33,17 @@ def essai():
     if request.method == 'POST':
         try:
             essai = int(request.form['essai'])
-        except ValueError:
-            message = "Please enter a valid number."
+            if not (1 <= essai <= 100):
+                raise ValueError("Le nombre doit être entre 1 et 100.")
+        except ValueError as e:
+            message = str(e)
         else:
             i += 1
             if essai == cible:
                 message = "WIN !!!"
-                time.sleep(3)
                 return render_template('index.html')
             elif i > 10:
                 message = "Lost..."
-                time.sleep(3)
                 return render_template('index.html')
             elif cible > essai:
                 message = "NOT ENOUGH..."
