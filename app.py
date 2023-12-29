684ea02fada440b40207c76641b296ef72661133
Wassim <115497440+ZayToxFF@users.noreply.github.com>
2023-12-29 20:54:20 +0100

    test fix

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def essai():
             else:
                 message = "TOO HIGH..."
 
-    return render_template('essai.html', i=i, pseudo=pseudo, message=message)
+    return render_template('essai.html', i=i, pseudo=pseudo, message=message, cible=cible)
 
 @app.errorhandler(Exception)
 def handle_error(e):
